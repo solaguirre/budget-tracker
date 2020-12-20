385c022b8027ab2bd79c80b0babc759bab246c47
solaguirre <68126593+solaguirre@users.noreply.github.com>
2020-12-20 00:33:03 -0700

    Updated webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+
+const config = {
+    entry: ".public/index.js",
+    output: {
+        path: __dirname + "/public/dist",
+        filename: "bundle.js",
+    },
+    mode: "development",
+    plugins: [
+        new WebpackPwaManifest({
+            name: "Budget Tracker",
+            short_name: "Budget Tracker",
+            description: "An application to track your expenses while on the go.",
+            background_color: "#01579b",
+            theme_color: "#ffffff",
+            start_url: "/",
+            icons: [
+                {
+                    src: path.resolve("public/icons/icon-192x192.png"),
+                    sizes: [192, 512],
+                    destination: path.join("assets", "icons"),
+                },
+            ],
+        }),
+    ],
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env"]
+                    }
+                }
+            }
+        ]
+    }
+};
+
+module.exports = config;
